5119000f9ab0634cf25c4e41873a7d1b0f982622
Aleksey074 <94966508+Aleksey074@users.noreply.github.com>
2022-03-26 20:22:42 +0500

    hw-2

---
+++ src/components/Message/Message.jsx
@@ -0,0 +1,14 @@
+import "./Message.style.css"
+
+export const Message = ({ author, text }) => {
+    return ( 
+    <div className="message">
+        <span className="author">{author}: </span>
+        <span className="text">{text}</span>
+    </div>
+    );
+};
+ 
+
+    
+    
