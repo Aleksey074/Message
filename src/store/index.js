5a5c6eee72895b9b4459331ab948c6d69bc25408
Aleksey074 <94966508+Aleksey074@users.noreply.github.com>
2022-04-12 14:09:49 +0500

    hw-6

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { combineReducers, createStore } from "redux"
+import { profileReducer } from "./profile/reducer";
+import { chatsReducer } from "./chats/reducer";
+import { messagesReducer } from "./messages/reducer";
+
+
+const rootReducer = combineReducers ({  //корневой reducer
+    profile: profileReducer,   // название раздела: фи-я reducer для раздела
+    chats: chatsReducer,
+    messages: messagesReducer,
+})
+
+export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); 
